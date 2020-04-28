e3f5c864c167151e74cc19c8b4936ba87a3e964e
Cody Drisko <cdrisko@nd.edu>
2020-04-28 08:44:06 -0400

    Fixed some name clashes in the cmake scripts

--- cmake/nd-research_utilities.cmake
+++ cmake/nd-research_utilities.cmake
@@ -1,7 +1,7 @@
 ### Functions ###
 
 ## Function to find and download external projects from Github ##
-function(externalDownload)
+function(nd-research_ExternalDownload)
     set(options)
     set(one_value_keywords PROJECT
                            REPOSITORY)
@@ -35,7 +35,7 @@ function(externalDownload)
 endfunction()
 
 ## Function to create a new test from a predefined naming template ##
-function(newTest)
+function(nd-research_NewTest)
     set(options)
     set(one_value_keywords TESTNAME
                            INTERIOR_DIRECTORY)
@@ -43,11 +43,11 @@ function(newTest)
 
     cmake_parse_arguments(TEST_ARGS "${options}" "${one_value_keywords}" "${multi_value_keywords}" ${ARGN})
 
-    add_executable(test${TEST_ARGS_TESTNAME}
+    add_executable(test${TEST_ARGS_TESTNAME}Functions
                    ${TEST_ARGS_INTERIOR_DIRECTORY}/test${TEST_ARGS_TESTNAME}.cpp ${NEW_SOURCES})
 
-    target_link_libraries(test${TEST_ARGS_TESTNAME} ${GTEST_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
+    target_link_libraries(test${TEST_ARGS_TESTNAME}Functions ${GTEST_LIBRARIES} ${CMAKE_THREAD_LIBS_INIT})
 
-    gtest_discover_tests(test${TEST_ARGS_TESTNAME}
+    gtest_discover_tests(test${TEST_ARGS_TESTNAME}Functions
                          WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/${TEST_ARGS_INTERIOR_DIRECTORY})
 endfunction()
