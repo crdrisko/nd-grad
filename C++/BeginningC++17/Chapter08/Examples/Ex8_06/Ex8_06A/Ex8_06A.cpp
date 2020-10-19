6a756a41643ce51c32c6a981600f07caee6eaa4f
crdrisko <cdrisko@nd.edu>
2020-10-19 06:50:01 -0400

    Renamed all book directories and chapter directories

---
+++ C++/BeginningC++17/Chapter08/Examples/Ex8_06/Ex8_06A/Ex8_06A.cpp
@@ -0,0 +1,35 @@
+// Copyright (c) 2018 Ivor Horton and Peter Van Weert. Some rights reserved.
+// Licensed under the Freeware License. See the LICENSE file in the project root for more information.
+//
+// Name: Ex8_06A.cpp
+// Author: crdrisko
+// Date: 08/12/2020-07:50:23
+// Description: Passing a two-dimensional array to a function - magic number replaced
+
+#include <array>                                            // For std::size()
+#include <iostream>
+
+double yield(const double values[][4], std::size_t n);
+
+int main()
+{
+    double beans[3][4] { { 1.0,  2.0,  3.0,  4.0 },
+                         { 5.0,  6.0,  7.0,  8.0 },
+                         { 9.0, 10.0, 11.0, 12.0 } };
+
+    std::cout << "Yield = " << yield(beans, std::size(beans)) << std::endl;
+}
+
+// Function to compute total yield
+double yield(const double array[][4], std::size_t size)
+{
+    double sum {};
+    for (std::size_t i {}; i < size; ++i)                   // Loop through rows
+    {
+        for (double val : array[i])                         // Loop through elements in a row
+        {
+            sum += val;
+        }
+    }
+    return sum;
+}
---
+++ C++/C++20Recipes/Chapter11/Recipe11_01/Listing11_02.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2020 J. Burton Browning and Bruce Sutherland. Some rights reserved.
+// Licensed under the Freeware License. See the LICENSE file in the project root for more information.
+//
+// Name: Listing11_02.cpp
+// Author: crdrisko
+// Date: 10/13/2020-07:17:49
+// Description: Creating a thread
+
+#include <iostream>
+#include <thread>
+
+void ThreadTask()
+{
+    for (unsigned int i {0}; i < 20; ++i)
+        std::cout << "Output from thread" << std::endl;
+}
+
+int main()
+{
+    const unsigned int numberOfProcessors {std::thread::hardware_concurrency()};
+
+    std::cout << "This system can run " << numberOfProcessors << " concurrent tasks" << std::endl;
+
+    if (numberOfProcessors > 1)
+    {
+        std::thread myThread {ThreadTask};
+        std::cout << "Output from main" << std::endl;
+        myThread.join();
+    }
+    else
+        std::cout << "CPU does not have multiple cores." << std::endl;
+
+    return 0;
+}
